07d3e103f5979a6a27765860169912037ee6460a
yaseenix <yassine.ammar@protonmail.com>
2022-01-13 21:11:59 +0100

    buil-Inbox-Contract

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,7 +1,18 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
 
 contract Inbox {
-  constructor() {
+  string public message; 
+
+  /**A constructor*/
+  constructor(string memory _message)  
+  {
+    message = _message;
+  }
+
+  function setMessage(string memory _message) 
+  public 
+  {
+    message = _message;
   }
 }
