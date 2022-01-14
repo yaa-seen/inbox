73f38c76fed40781bfbc70ca305e7941cb9cd6a0
yaseenix <yassine.ammar@protonmail.com>
2022-01-14 23:12:24 +0100

    change indentation

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity 0.8.11;
+
 
 contract Inbox {
-  string public message; 
+    string public message;
 
   /**A constructor*/
-  constructor(string memory _message)  
-  {
-    message = _message;
-  }
+    constructor(string memory _message) public {
+        message = _message;
+    }
 
-  function setMessage(string memory _message) 
-  public 
-  {
-    message = _message;
-  }
+    function setMessage(string memory _message)
+    public
+    {
+        message = _message
+    }
 }
